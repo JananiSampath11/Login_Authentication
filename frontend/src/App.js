e46131df09765366bd69188362a9168298ff0deb
ShriJanani <shrijanani.s@secqureone.com>
2023-10-11 15:11:13 +0530

    baic setup

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}></Route>
+          <Route exact path="/register" element={<Register/>}></Route>
+          <Route exact path="/forgotpassword" element={<ForgotPassword/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
