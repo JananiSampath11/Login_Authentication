4a1a6b2f8cc5bf70e97384e36faf8109fe8fab61
ShriJanani <shrijanani.s@secqureone.com>
2023-10-11 15:46:40 +0530

    basic setup

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,11 +1,88 @@
-import React from 'react'
+// import React from 'react'
 
-const Register = () => {
+// const Register = () => {
+//   return (
+//     <div>
+//       register page
+//     </div>
+//   )
+// }
+
+// export default Register
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+function Register() {
+  const history = useNavigate();
+  const [name,setName]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function Submit(e) {
+    e.preventDefault();
+    try {
+      await axios
+        .post("http://localhost:5000/", {
+          name,  
+          email,
+          password,
+        })
+        .then((res) => {
+          if ((res.data = "exist")) {
+            alert("user already exist");
+            // history("/home", { state: { id: email } });
+          } else if ((res.data = "no exist")) {
+            history("/home", { state: { id: email } });
+          }
+        })
+        .catch((e) => {
+          alert("wrong detail");
+          console.log(e);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <div>
-      register page
-    </div>
-  )
-}
+    <div className="Login">
+      <h1>Login</h1>
+      <form action="POST">
+      <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Username"
+          name=""
+          id=""
+        />
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+          name=""
+          id=""
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+          name=""
+          id=""
+        />
 
-export default Register
+        <input type="submit" onClick={Submit} />
+      </form>
+      <p>OR</p>
+      <br />
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
+export default Register;
